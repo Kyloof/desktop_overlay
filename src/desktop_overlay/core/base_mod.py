c1ce7fe4366f43c383b9425d44fc0fb59fd97520
Kyloof <jankubow.sr@gmail.com>
2025-06-19 02:00:44 +0200

    mod_manager update

--- src/desktop_overlay/core/base_mod.py
+++ src/desktop_overlay/core/base_mod.py
@@ -1,28 +1,65 @@
-from abc import ABC, abstractmethod
+from abc import ABC, ABCMeta, abstractmethod
 from PySide6.QtWidgets import QWidget
 
+QWidgetMeta = type(QWidget)
 
-class BaseMod(ABC, QWidget):
+class _BaseModMeta(ABCMeta, QWidgetMeta):
+    '''
+    Create meta that connects ABCMeta and QWidgetMeta here cuz python
+    '''
+    pass
+
+class BaseMod(ABC, QWidget, metaclass=_BaseModMeta):
+    '''
+    Abstract class for all mods that are yet to come
+    '''
+
     @abstractmethod
     def load(self):
+        '''
+        Load the mod into the overlay, but dont run it just yet..
+        Should preferably get it's own thread???
+        '''
         pass
 
     @abstractmethod
     def unload(self):
+        '''
+        Unload the mod from the overlay
+        '''
         pass
 
     @abstractmethod
     def run(self):
+        '''
+        Finally, run the mod:DD
+        Or maybe assign thread here more likely
+        '''
         pass
 
     @abstractmethod
     def stop(self):
+        '''
+        Stop the mod, f.e when the overlay is minimalized
+        Put the thread to sleep i think
+        '''
         pass
 
     @abstractmethod
     def resume(self):
+        '''
+        Resume the mod, f.e when the overlay is maximized, and you had it opened previously
+        Wake up the thread
+        '''
         pass
 
+    #TODO: SETTINGS
+    '''
     @abstractmethod
-    def settings(self) -> QWidget:
+    def settings(self):
+        
+        Mod settings
+        For now TODO
+        
         pass
+    '''
--- src/desktop_overlay/core/mod_manager.py
+++ src/desktop_overlay/core/mod_manager.py
@@ -1,15 +1,51 @@
-from PySide6.QtWidgets import QWidget
-from core.base_mod import BaseMod
+from .base_mod import BaseMod
 import os
 import importlib
 
 
 class ModManager:
+    '''
+    The Mod Manager. 
+    Mod Manager detects and loads BaseMod subclasses from your mod packages and, if they're not fucked up, 
+    after refreshing mods, should display them in installed mods section in UI. 
+    There you can enable or disable them which would invoke the load or unload mod function in ModManager.
+    '''
     def __init__(self):
-        self.mods: List[BaseMod] = []
-        self.enabled_mods: List[BaseMod] = []
-        self.MODS_PATH = "mods"
+        #NOTE: mods[False] -> disabled mods, mods[True] -> enabled mods
+        self.mods: dict[bool, dict[int, BaseMod]] = {}
+        self.BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.MODS_PATH = os.path.abspath(os.path.join(self.BASE_DIR, '..', 'mods'))
 
     def detect_mods(self) -> None:
+        '''
+        Detect installed mod packages from the mod folder.
+        '''
+        mod_id = 0
         for path in os.listdir(self.MODS_PATH):
-            if path.endswith(".py"):
+            mod_path = os.path.join(self.MODS_PATH, path)
+            if os.path.isdir(mod_path) and mod_path.endswith("mod"):
+                try:
+                    module = importlib.import_module(path)
+                except ModuleNotFoundError as e:
+                    print(f"failed to import {path}: {e}")
+                    continue
+            #TODO:detect subclasses of BaseMod from a module and save them into the mods dictionary
+
+    def load_mod(self, mod_id: int):
+        '''
+        Load the mod and make it work:D
+        '''
+        if mod_id in self.mods[False]:
+            module = self.mods[False].pop(mod_id)
+            module.load()
+            self.mods[True][mod_id] = module
+
+    def unload_moad(self, mod_id: int):
+        '''
+        Unload the mod and stop the mod:c
+        '''
+        if mod_id in self.mods[True]:
+            module = self.mods[True].pop(mod_id)
+            module.unload()
+            self.mods[False][mod_id] = module
+

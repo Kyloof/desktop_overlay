6a0c8049b78bb6cbf3f4202dc40c8ae6f1166923
Kyloof <jankubow.sr@gmail.com>
2025-08-02 10:46:55 +0200

    split up config into multiple files

--- tests/test_hotkey_manager.py
+++ tests/test_hotkey_manager.py
@@ -2,14 +2,17 @@ import pytest
 from unittest.mock import Mock
 from desktop_overlay.core.hotkey_manager import HotkeyManager
 
+
 @pytest.fixture
 def hotkey_manager():
     return HotkeyManager(Mock())
+
 
 def test_save_new_sequence(hotkey_manager):
     new_seq = {Mock(), Mock()}
     hotkey_manager.save_new_sequence(new_seq)
     assert hotkey_manager.activation_sequence == new_seq
+
 
 def test_change_activation_sequence(hotkey_manager):
     result = hotkey_manager.change_activation_sequence()
--- tests/test_settings_manager.py
+++ tests/test_settings_manager.py
@@ -2,6 +2,7 @@ import pytest
 from unittest.mock import Mock, patch
 from desktop_overlay.core.settings_manager import SettingsManager
 
+
 @pytest.fixture
 def mock_screens():
     screen1 = Mock()
@@ -16,40 +17,49 @@ def mock_screens():
 
     return [screen1, screen2]
 
+
 @pytest.fixture
 def settings_manager():
     return SettingsManager(Mock())
+
 
 def test_init(settings_manager):
     assert settings_manager.screens == []
     assert settings_manager.active_screen is None
 
+
 def test_change_overlay_shortcut(settings_manager):
     settings_manager.change_overlay_shortcut()
     settings_manager.hotkey_manager.change_activation_sequence.assert_called_once()
+
 
 def test_setup_screens(settings_manager, mock_screens):
     settings_manager.setup_screens(mock_screens, 0)
     assert settings_manager.active_screen == mock_screens[0]
 
+
 def test_setup_screens_invalid_index(settings_manager, mock_screens):
     settings_manager.setup_screens(mock_screens, 10)
     assert settings_manager.active_screen is None
+
 
 def test_list_screens(settings_manager, mock_screens):
     settings_manager.screens = mock_screens
     result = settings_manager.list_screens()
     assert result == [(0, mock_screens[0]), (1, mock_screens[1])]
 
+
 def test_get_screens_strings(settings_manager, mock_screens):
     settings_manager.screens = mock_screens
     result = settings_manager.get_screens_strings()
     assert result == ["  0 - Screen 1", "  1 - Screen 2"]
 
+
 def test_get_screen_geometry_success(settings_manager, mock_screens):
     settings_manager.active_screen = mock_screens[0]
     geometry, x, y = settings_manager._get_screen_geometry()
     assert x == 100 and y == 200
+
 
 def test_get_screen_geometry_no_active_screen(settings_manager):
     with pytest.raises(RuntimeError, match="No active screen selected."):
